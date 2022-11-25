5cd2fa17f68e69a2478539c9f4f65ae0705c5e9c
Kylie <kylieavie@gmail.com>
2022-11-25 14:44:46 +0800

    questions project done

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,28 @@
 //using selectors inside the element
+const questions = document.querySelectorAll(".question");
+
+questions.forEach((question) => {
+  console.log(question);
+  const btn = question.querySelector(".question-btn");
+  console.log(btn);
+  btn.addEventListener("click", () => {
+    questions.forEach((qn) => {
+      console.log(qn);
+      if (qn !== question) {
+        qn.classList.remove("show-text");
+      }
+    });
+    question.classList.toggle("show-text");
+  });
+});
+
 // traversing the dom
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     console.log(e.currentTarget.parentElement.parentElement);
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text");
+//   });
+// });
